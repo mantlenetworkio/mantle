c25651ce11456156f23027ac995f8393f3a77664
Shi Wenbin <bradyjoestar@gmail.com>
2022-08-11 20:49:20 +0800

    reuse pre-deployed contracts on goerli (#6)
    * reusing pre-deployment contracts on goerli
    
    * add pre-deployed contracts
    
    * init base balance
    
    Co-authored-by: wenbin <wenbin.shi@gmail.com>

---
+++ packages/contracts/deploy-config/goerlibn.ts
@@ -0,0 +1,19 @@
+const config = {
+  numDeployConfirmations: 1,
+  l1BlockTimeSeconds: 15,
+  l2BlockGasLimit: 15_000_000,
+  l2ChainId: 1705003,
+  ctcL2GasDiscountDivisor: 32,
+  ctcEnqueueGasCost: 60_000,
+  sccFaultProofWindowSeconds: 10,
+  sccSequencerPublishWindowSeconds: 12592000,
+  bvmSequencerAddress: '0xCd0B4E309FB855d644bA64E5fb3dC3DD08f13917',
+  bvmProposerAddress: '0x24280Da7E82ab0Ec4E38B394aCF0196c21663B75',
+  //default ovmBlockSignerAddress
+  bvmBlockSignerAddress: '0x00000398232E2064F896018496b4b44b3D62751F',
+  bvmFeeWalletAddress: '0xCd0B4E309FB855d644bA64E5fb3dC3DD08f13917',
+  bvmAddressManagerOwner: '0xCd0B4E309FB855d644bA64E5fb3dC3DD08f13917',
+  bvmGasPriceOracleOwner: '0xCd0B4E309FB855d644bA64E5fb3dC3DD08f13917',
+}
+
+export default config
