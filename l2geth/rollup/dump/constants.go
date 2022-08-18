ddd08b646a732547b3bb13da10873b4c866d7373
curryxbo <curryx1992@gmail.com>
2022-08-18 22:42:30 +0800

    add tssRewardContract addr

--- l2geth/rollup/dump/constants.go
+++ l2geth/rollup/dump/constants.go
@@ -7,7 +7,6 @@ import (
 var BvmBitAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000")
 var BvmFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
 
-// TODO: set contract addr
-var L1ExcuteFeeWallet = common.HexToAddress("")
-var L2ExcuteFeeWallet = common.HexToAddress("")
+var L1ExcuteFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
+var L2ExcuteFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000020")
 var BvmWhitelistAddress = common.HexToAddress("0x4200000000000000000000000000000000000002")
