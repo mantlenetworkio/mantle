5caad686de265fa54b5947758b5b427f85ee6d93
sha3n <qingruishen@yahoo.com>
2022-08-22 11:00:12 +0800

    [feat]: imply balance check

--- packages/contracts/contracts/L2/predeploys/TssRewardContract.sol
+++ packages/contracts/contracts/L2/predeploys/TssRewardContract.sol
@@ -22,6 +22,7 @@ contract TssRewardContract is ITssRewardContract {
     address payable public owner;
     uint256 public bestBlockID = 0;
     uint256 public dust = 0;
+    uint256 public totalAmount = 0;
 
     // set call address
     constructor(address _deadAddress, address payable _owner) {
@@ -49,10 +50,18 @@ contract TssRewardContract is ITssRewardContract {
         _;
     }
 
+    modifier checkBalance() {
+        require(
+            address(this).balance == totalAmount,
+            "balance record and contract balance are not equal"
+        );
+        _;
+    }
+
     /**
      * @dev return the total undistributed amount
      */
-    function queryReward() external view returns (uint256) {
+    function queryReward() external view checkBalance returns (uint256) {
         return address(this).balance;
     }
 
@@ -63,25 +72,27 @@ contract TssRewardContract is ITssRewardContract {
      * @param _tssMembers The address array of tss group members
      */
     function claimReward(uint256 _blockStartHeight, uint32 _length, address[] calldata _tssMembers)
-    external
-    virtual
-    onlyFromDeadAddress
+        external
+        virtual
+        onlyFromDeadAddress
+        checkBalance
     {
-        uint256 totalAmount = ledger[_blockStartHeight];
+        uint256 blockAmount = ledger[_blockStartHeight];
         uint256 sendAmount = 0;
         uint256 accu = 0;
         // release reward from _blockStartHeight to _blockStartHeight + _length
         for (uint256 i=_blockStartHeight; i<_length; i++) {
             // calc send amount
-            sendAmount = totalAmount / _tssMembers.length; // TODO safemath
+            sendAmount = blockAmount / _tssMembers.length; // TODO safemath
             // delete distributed height
             delete ledger[_blockStartHeight];
             for (uint256 j=0; j < _tssMembers.length; j++) {
                 address payable addr = payable(_tssMembers[j]);
                 accu += sendAmount;
+                totalAmount -= sendAmount;
                 addr.transfer(sendAmount);
             }
-            uint256 reserved = totalAmount - accu;
+            uint256 reserved = blockAmount - accu;
             require(reserved >= 0, "release amount gt real balance");
             if (reserved > 0) {
                 dust += reserved;
@@ -102,30 +113,32 @@ contract TssRewardContract is ITssRewardContract {
      * @return _tssMembers Address array of tss group members
      */
     function updateReward(uint256 _blockID, uint256 _amount)
-    external
-    payable
-    onlyFromDeadAddress
-    returns (bool)
+        external
+        payable
+        onlyFromDeadAddress
+        checkBalance
+        returns (bool)
     {
-        // TODO check balance, require ledger sum balance match to contract global balance
-
         // check update block ID
         require(_blockID == bestBlockID+1, "block id update illegal");
         // check transfer amount
         require(msg.value == _amount, "transfer amount and update amount not equal");
         // iter address to update balance
-        ledger[_blockID] = _amount;
         bestBlockID = _blockID;
+        totalAmount += _amount;
+        ledger[_blockID] = _amount;
         return true;
     }
 
-    function withdrawDust() external onlyOwner {
+    function withdrawDust() external onlyOwner checkBalance {
         uint256 amount = dust;
         dust = 0;
+        totalAmount -= dust;
         owner.transfer(amount);
     }
 
-    function withdraw() external onlyOwner {
+    function withdraw() external onlyOwner checkBalance {
+        totalAmount = 0;
         owner.transfer(address(this).balance);
     }
 }
