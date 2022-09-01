ad05c6134aa832e9b7430dad026543648f3377b4
kukoo <96556724+Kukoomomo@users.noreply.github.com>
2022-09-01 11:57:39 +0800

    Tss staking contract (#68)
    * tss group contract code
    
    * tss contract adjust
    
    * add staking & slashing contract , update tss group contract (#67)
    
    * update test
    
    * merge tssgroup
    
    * add make file
    
    * add inactive member interface
    
    * update transformDeposit function
    
    * change quit request can call any time
    
    
    * change tssgroup contract for many election
    
    Co-authored-by: guoshijiang <guoshijiang2012@163.com>

---
+++ packages/contracts/contracts/L1/tss/ITssStakingSlashing.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.5.0 <0.9.0;
+
+interface IStakingSlashing {
+    struct DepositInfo {
+        bytes pubKey;
+        uint256 amount;
+    }
+
+    function staking(uint256, bytes memory) external;
+    function withdrawToken() external;
+    function quit() external;
+    function clearQuitList() external;
+    function slashing(bytes memory, bytes memory) external;
+    function getDeposits(address) external returns (DepositInfo memory);
+    function getSlashRecord(uint256, address) external returns (bool);
+}
--- packages/contracts/src/address-names.ts
+++ packages/contracts/src/address-names.ts
@@ -12,6 +12,8 @@ export const names = {
       BVM_L1CrossDomainMessenger: 'BVM_L1CrossDomainMessenger',
       Proxy__BVM_L1CrossDomainMessenger: 'Proxy__BVM_L1CrossDomainMessenger',
       Proxy__BVM_L1StandardBridge: 'Proxy__BVM_L1StandardBridge',
+      TssGroupManager: 'TssGroupManager',
+      TssStakingSlashing:'TssStakingSlashing',
     },
     accounts: { BVM_Sequencer: 'BVM_Sequencer', BVM_Proposer: 'BVM_Proposer' },
   },
