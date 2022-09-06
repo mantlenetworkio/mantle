0b9151358e927810ac35ceca8f338a390c491401
chengwenxi <22697326+chengwenxi@users.noreply.github.com>
2022-09-06 16:44:36 +0800

    fix contracts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -52,11 +52,13 @@ const config: HardhatUserConfig = {
       url: 'http://127.0.0.1:8545',
       accounts: [privateKey],
     },
-    'goerlibn': {
+    goerlibn: {
       chainId: 5,
       url: 'https://eth-goerli.g.alchemy.com/v2/821_LFssCCQnEG3mHnP7tSrc87IQKsUp',
       deploy,
-      accounts: ['6395a7c842a08515961888d21d72f409b61fbce96af1e520384e375f301a8297'],
+      accounts: [
+        '6395a7c842a08515961888d21d72f409b61fbce96af1e520384e375f301a8297',
+      ],
     },
     'goerli-qa': {
       chainId: 5,
