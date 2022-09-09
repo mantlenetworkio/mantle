410eea9b7e41ccf50e190b69a9ed6b811fc6a2fd
FletcherMan <fancituer@163.com>
2022-09-09 12:40:16 +0800

    fix tss tasks

--- packages/contracts/tasks/tss-tasks.ts
+++ packages/contracts/tasks/tss-tasks.ts
@@ -1,44 +1,64 @@
-import { task } from 'hardhat/config'
-import { getContractFactory } from '../src/contract-defs'
+import {task} from 'hardhat/config'
+import {getContractFactory} from '../src/contract-defs'
+import {HexToBytes} from "../src/deploy-utils";
+import {ethers} from "ethers";
 
 
 task("setTssGroupMember")
   .addParam('contract', "tss group contract address")
   .addParam('threshold', "tss threshold")
-  .addParam('batch_publicKey', "tss group batch publicKey")
+  .addParam('batchpublickey', "tss group batch publicKey")
   .setAction(async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
     const tssGroupManager = getContractFactory('TssGroupManager').attach(taskArgs.contract)
     const thresholdP = taskArgs.threshold
-    const batchPublicKey = taskArgs.batch_publicKey
-    await tssGroupManager.setTssGroupMember(thresholdP, batchPublicKey)
+
+    const batchPublicKey = []
+    //support parse special array data for batchpublickey
+    let re = /\[/gi
+    taskArgs.batchpublickey = taskArgs.batchpublickey.replace(re, '')
+    re = /\]/gi
+    taskArgs.batchpublickey = taskArgs.batchpublickey.replace(re, '')
+    taskArgs.batchpublickey = taskArgs.batchpublickey.split(',')
+    for (const pk of taskArgs.batchpublickey) {
+      const ret = await HexToBytes(pk)
+      batchPublicKey.push(ret)
+    }
+    await tssGroupManager.connect(accounts[0]).setTssGroupMember(thresholdP, batchPublicKey)
   })
 
-
 task("setSlashingParams")
-    .addParam('contract', "tss staking slashing contract address")
-    .addParam('slashamount0', "slash uptime amount")
-    .addParam('slashamount1', "slash animus amount")
-    .addParam('exincome0', "slash uptime extra income")
-    .addParam('exincome1', "slash animus extra income")
-    .setAction(async (taskArgs, hre) => {
-        const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
-        let slashParams = [taskArgs.slashamount0, taskArgs.slashamount1]
-        let exIncomes = [taskArgs.exincome0, taskArgs.exincome1]
-        await tssStakingSlashing.setSlashingParams(slashParams, exIncomes)
-    })
+  .addParam('contract', "tss staking slashing contract address")
+  .addParam('slashamount0', "slash uptime amount")
+  .addParam('slashamount1', "slash animus amount")
+  .addParam('exincome0', "slash uptime extra income")
+  .addParam('exincome1', "slash animus extra income")
+  .setAction(async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+    const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
+    let slashParams = [taskArgs.slashamount0, taskArgs.slashamount1]
+    let exIncomes = [taskArgs.exincome0, taskArgs.exincome1]
+    await tssStakingSlashing.connect(accounts[0]).setSlashingParams(slashParams, exIncomes)
+  })
 
 task("clearQuitRequestList")
-    .addParam('contract', "tss staking slashing contract address")
-    .setAction(async (taskArgs, hre) => {
-        const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
-        await tssStakingSlashing.clearQuitRequestList()
-    })
+  .addParam('contract', "tss staking slashing contract address")
+  .setAction(async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+    const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
+    await tssStakingSlashing.connect(accounts[0]).clearQuitRequestList()
+  })
 
 task("staking")
-    .addParam('contract', "tss staking slashing contract address")
-    .addParam('amount')
-    .addParam('pubkey')
-    .setAction(async (taskArgs, hre) => {
-        const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
-        await tssStakingSlashing.staking(taskArgs.amount, taskArgs.pubkey)
-    })
+  .addParam('contract', "tss staking slashing contract address")
+  .addParam('amount')
+  .addParam('pubkey')
+  .addParam('prikey')
+  .setAction(async (taskArgs, hre) => {
+    const signer = new hre.ethers.Wallet(
+      taskArgs.prikey,
+      hre.ethers.provider
+    )
+    const tssStakingSlashing = getContractFactory('TssStakingSlashing').attach(taskArgs.contract)
+    await tssStakingSlashing.connect(signer).staking(taskArgs.amount, taskArgs.pubkey)
+  })
