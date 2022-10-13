aec2defd31b9e955946704cbf0af79c0440c0c07
curryxbo <curryx1992@gmail.com>
2022-10-13 16:53:52 +0800

    clean

--- l2geth/core/state_transition.go
+++ l2geth/core/state_transition.go
@@ -23,12 +23,10 @@ import (
 
 	"github.com/mantlenetworkio/mantle/l2geth/common"
 	"github.com/mantlenetworkio/mantle/l2geth/common/hexutil"
-	"github.com/mantlenetworkio/mantle/l2geth/contracts/tssreward"
 	"github.com/mantlenetworkio/mantle/l2geth/core/types"
 	"github.com/mantlenetworkio/mantle/l2geth/core/vm"
 	"github.com/mantlenetworkio/mantle/l2geth/log"
 	"github.com/mantlenetworkio/mantle/l2geth/params"
-	"github.com/mantlenetworkio/mantle/l2geth/rollup/dump"
 	"github.com/mantlenetworkio/mantle/l2geth/rollup/fees"
 	"github.com/mantlenetworkio/mantle/l2geth/rollup/rcfg"
 )
@@ -272,22 +270,16 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 	}
 	st.refundGas()
 	if rcfg.UsingBVM {
-		st.state.AddBalance(dump.BvmFeeWallet, st.l1Fee)
+		// The L2 Fee is the same as the fee that is charged in the normal geth
+		// codepath. Add the L1 fee to the L2 fee for the total fee that is sent
+		// to the sequencer.
+		l2Fee := new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice)
+		fee := new(big.Int).Add(st.l1Fee, l2Fee)
+		st.state.AddBalance(evm.Coinbase, fee)
+	} else {
+		st.state.AddBalance(evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
 	}
-	l2Fee := new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice)
-	st.state.AddBalance(dump.TssRewardAddress, l2Fee)
-	data, err := tssreward.PacketData(evm.BlockNumber, l2Fee)
-	if err != nil {
-		return nil, 0, false, err
-	}
-	deadAddress := vm.AccountRef(dump.DeadAddress)
-	log.Info("update reward ........")
-	_, _, err = evm.Call(deadAddress, dump.TssRewardAddress, data, 210000, big.NewInt(0))
-	if err != nil {
-		log.Error("update reward in error: ", err)
-		return nil, 0, false, err
-	}
-	log.Info("reward updated ........")
+
 	return ret, st.gasUsed(), vmerr != nil, err
 }
 
