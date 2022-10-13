6b54f3ae9e91e162e40c5d861b9a27e1f783834d
curryxbo <curryx1992@gmail.com>
2022-10-13 17:09:18 +0800

    fix zeroAddress

--- l2geth/core/state_transition.go
+++ l2geth/core/state_transition.go
@@ -283,7 +283,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		if err != nil {
 			return nil, 0, false, err
 		}
-		zeroAddress := vm.AccountRef(common.Address{})
+		zeroAddress := vm.AccountRef(dump.DeadAddress)
 		_, _, err = evm.Call(zeroAddress, dump.BvmFeeWallet, data, 210000, big.NewInt(0))
 	} else {
 		st.state.AddBalance(evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
