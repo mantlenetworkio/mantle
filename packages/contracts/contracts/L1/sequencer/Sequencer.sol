334a43c12edacbf03ab3178a237e8942bd715cde
kukoomomo <kukoomomo@gmail.com>
2022-10-14 14:50:22 +0800

    update change name

--- packages/contracts/contracts/L1/sequencer/Sequencer.sol
+++ packages/contracts/contracts/L1/sequencer/Sequencer.sol
@@ -10,8 +10,8 @@ import "./ISequencer.sol";
 contract Sequencer is ISequencer, ReentrancyGuardUpgradeable, OwnableUpgradeable {
     using SafeERC20 for IERC20;
 
-    mapping(address => Sequencer) public seqsDeposit;
-    address[] public depositKeys;
+    mapping(address => Sequencer) public sequencers;
+    address[] public owners;
     address bitToken;
 
     function initialize(address _bitToken) public initializer {
@@ -35,57 +35,57 @@ contract Sequencer is ISequencer, ReentrancyGuardUpgradeable, OwnableUpgradeable
         require(_mintAddress != address(0), "invild address, address can not be 0");
 
         // check already have sequencer
-        require(seqsDeposit[msg.sender].mintAddress == address(0), "already have deposit");
+        require(sequencers[msg.sender].mintAddress == address(0), "already have deposit");
         IERC20(bitToken).safeTransferFrom(msg.sender, address(this), _amount);
 
-        uint256 index = depositKeys.length;
-        seqsDeposit[msg.sender] = Sequencer({
+        uint256 index = owners.length;
+        sequencers[msg.sender] = Sequencer({
             owner: msg.sender,
             mintAddress: _mintAddress,
             nodeID: _nodeID,
             amount: _amount,
             keyIndex: index
         });
-        depositKeys[index] = msg.sender;
+        owners[index] = msg.sender;
     }
 
     function deposit(uint256 _amount) external nonReentrant {
         // check params
         require(_amount > 0, "invild amount");
         // check already have sequencer
-        require(seqsDeposit[msg.sender].mintAddress != address(0), "do not have create");
+        require(sequencers[msg.sender].mintAddress != address(0), "do not have create");
 
         // transfer
         IERC20(bitToken).safeTransferFrom(msg.sender, address(this), _amount);
-        seqsDeposit[msg.sender].amount += _amount;
+        sequencers[msg.sender].amount += _amount;
     }
 
     function withdraw(uint256 _amount) external nonReentrant {
         // check params
         require(_amount > 0, "invild amount");
         // check already have sequencer
-        require(seqsDeposit[msg.sender].mintAddress != address(0), "do not have create");
+        require(sequencers[msg.sender].mintAddress != address(0), "do not have create");
 
         uint256 withdrawAmount = _amount;
-        if (_amount > seqsDeposit[msg.sender].amount) {
-            // when _amount > seqsDeposit.amount, withdraw all tokens
-            withdrawAmount = seqsDeposit[msg.sender].amount;
+        if (_amount > sequencers[msg.sender].amount) {
+            // when _amount > sequencers.amount, withdraw all tokens
+            withdrawAmount = sequencers[msg.sender].amount;
         }
 
         // transfer
         IERC20(bitToken).safeTransfer(msg.sender, withdrawAmount);
 
-        seqsDeposit[msg.sender].amount -= withdrawAmount;
-        if (seqsDeposit[msg.sender].amount == 0) {
+        sequencers[msg.sender].amount -= withdrawAmount;
+        if (sequencers[msg.sender].amount == 0) {
             deleteSequencer(msg.sender);
         }
     }
 
     function withdrawAll() external nonReentrant {
         // check already have sequencer
-        require(seqsDeposit[msg.sender].mintAddress != address(0), "do not have create");
+        require(sequencers[msg.sender].mintAddress != address(0), "do not have create");
 
-        uint256 withdrawAmount = seqsDeposit[msg.sender].amount;
+        uint256 withdrawAmount = sequencers[msg.sender].amount;
 
         // transfer
         IERC20(bitToken).safeTransfer(msg.sender, withdrawAmount);
@@ -93,29 +93,29 @@ contract Sequencer is ISequencer, ReentrancyGuardUpgradeable, OwnableUpgradeable
     }
 
     function getSequencers() external view returns (Sequencer[] memory) {
-        Sequencer[] memory seqs = new Sequencer[](depositKeys.length);
-        for (uint256 i = 0; i < depositKeys.length; i++) {
-            address key = depositKeys[i];
-            seqs[i] = seqsDeposit[key];
+        Sequencer[] memory seqs = new Sequencer[](owners.length);
+        for (uint256 i = 0; i < owners.length; i++) {
+            address key = owners[i];
+            seqs[i] = sequencers[key];
         }
         return seqs;
     }
 
     function getSequencer(address signer) external view returns (Sequencer memory) {
-        return seqsDeposit[signer];
+        return sequencers[signer];
     }
 
     function isSequencer(address signer) external view returns (bool) {
-        return seqsDeposit[signer].mintAddress != address(0);
+        return sequencers[signer].mintAddress != address(0);
     }
 
     function deleteSequencer(address signer) internal {
-        uint256 index = seqsDeposit[signer].keyIndex;
-        uint256 length = depositKeys.length;
+        uint256 index = sequencers[signer].keyIndex;
+        uint256 length = owners.length;
 
         // delete
-        depositKeys[index] = depositKeys[length - 1];
-        delete depositKeys[length - 1];
-        delete seqsDeposit[signer];
+        owners[index] = owners[length - 1];
+        delete owners[length - 1];
+        delete sequencers[signer];
     }
 }
