ef1de5a0c03b933f87dcfd846ae62327bf117b5d
chengwenxi <22697326+chengwenxi@users.noreply.github.com>
2022-10-18 18:04:51 +0800

    fix compiling error

--- l2geth/consensus/clique/api.go
+++ l2geth/consensus/clique/api.go
@@ -8,3 +8,11 @@ type API struct {
 	chain  consensus.ChainReader
 	clique *Clique
 }
+
+// Producers returns the current producer the node tries to produce block.
+func (api *API) Producers() Producers {
+	api.clique.lock.RLock()
+	defer api.clique.lock.RUnlock()
+
+	return api.clique.producers
+}
