65f0afb5e41217c180c2ec29085fc0a2eb27ead8
curryxbo <curryx1992@gmail.com>
2022-10-20 16:41:55 +0800

    reset l2geth code

--- l2geth/metrics/metrics.go
+++ l2geth/metrics/metrics.go
@@ -17,7 +17,7 @@ import (
 // Enabled is checked by the constructor functions for all of the
 // standard metrics. If it is true, the metric returned is a stub.
 //
-// This global kill-switch helps quantify the payer effect and makes
+// This global kill-switch helps quantify the observer effect and makes
 // for less cluttered pprof profiles.
 var Enabled = false
 
