e7a2b9df4fcc3dbab26f39fce5a592bcad97e200
wenbin <wenbin.shi@gmail.com>
2022-10-26 19:03:43 +0800

    cancle init balance

--- ops/scripts/deployer.sh
+++ ops/scripts/deployer.sh
@@ -72,7 +72,13 @@ mv addresses.json ./genesis
 cp ./genesis/$CONTRACTS_TARGET_NETWORK.json ./genesis/state-dump.latest.json
 
 # init balance
-jq -n 'reduce inputs as $item ({}; . *= $item)' ./genesis/state-dump.latest.json ./balance.json > genesis2.json
+if [ $CONTRACTS_TARGET_NETWORK == "local" ] ;then
+  jq -n 'reduce inputs as $item ({}; . *= $item)' ./genesis/state-dump.latest.json ./balance.json > genesis2.json
+else [ $CONTRACTS_TARGET_NETWORK == "goerli-qa" ]
+  jq -n 'reduce inputs as $item ({}; . *= $item)' ./genesis/state-dump.latest.json ./balance.json > genesis2.json
+fi
+
+
 mv ./genesis/state-dump.latest.json ./genesis/state-dump1.latest.json
 mv ./genesis2.json ./genesis/state-dump.latest.json
 
