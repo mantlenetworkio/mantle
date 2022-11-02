e672fbeae6a4e9bdc12d2e95b2b58429d129508b
curryxbo <curryx1992@gmail.com>
2022-11-02 16:22:08 +0800

    fix _distributeReward

--- packages/contracts/contracts/L1/rollup/StateCommitmentChain.sol
+++ packages/contracts/contracts/L1/rollup/StateCommitmentChain.sol
@@ -354,12 +354,11 @@ contract StateCommitmentChain is IStateCommitmentChain, Lib_AddressResolver, Cro
     // 0x9d72b1e94c7075be7e6da0e2104db4302d02db0e
     function _distributeReward() public {
         // get address of tss group member
-        address[] memory tssMembers = new address[](2);
-        tssMembers[0] = address(0xCFc17379Ac80A9EF231772ACE60014fb84704cB4);
-        tssMembers[1] = address(0x9D72b1e94C7075Be7E6da0E2104DB4302d02DB0E);
-//        tssMembers.push(0XCFC17379AC80A9EF231772ACE60014FB84704CB4);
-//        tssMembers.push(0x9d72b1e94c7075be7e6da0e2104db4302d02db0e);
-//        require(tssMembers.length > 0, "get tss members in error");
+//        address[] memory tssMembers = new address[](2);
+//        tssMembers[0] = address(0xCFc17379Ac80A9EF231772ACE60014fb84704cB4);
+//        tssMembers[1] = address(0x9D72b1e94C7075Be7E6da0E2104DB4302d02DB0E);
+        address[] memory tssMembers = ITssGroupManager(resolve("TssGroupManager")).getTssGroupUnJailMembers();
+        require(tssMembers.length > 0, "get tss members in error");
         bytes32[] memory _batch;
         uint256 _shouldStartAtElement;
         // construct calldata for claimReward call
