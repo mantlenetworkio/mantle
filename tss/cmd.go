ca5711c4ab3ba73af3deeb304cd1871260c7f4c0
Jay.L <liujunyansuc@gmail.com>
2022-11-02 11:58:42 +0800

    change command

--- tss/cmd.go
+++ tss/cmd.go
@@ -19,41 +19,31 @@ func main() {
 		),
 	)
 
-	cmd := &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:   "tss",
-		Short: "Tss Command",
-	}
-
-	subCmd := &cobra.Command{
-		Use:   "start",
 		Short: "Tss Start Daemon",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			cfgFile, _ := cmd.Flags().GetString("config")
 			loadedCfg, err := common.LoadConfig(cfgFile)
-			if err != nil {
-				log.Error("fail to load config", err)
-				return err
+			if err == nil {
+				return common.SetCmdConfig(cmd, loadedCfg)
+			} else {
+				return nil
 			}
-
-			return common.SetCmdConfig(cmd, loadedCfg)
 		},
 	}
 
-	subCmd.AddCommand(
+	rootCmd.AddCommand(
 		manager.Command(),
 		tssnode.Command(),
-	)
-
-	cmd.AddCommand(
-		subCmd,
 		tssnode.PeerIDCommand(),
 	)
 
-	subCmd.PersistentFlags().StringP("config", "c", "config", "configuration file with extension")
+	rootCmd.PersistentFlags().StringP("config", "c", "config", "configuration file with extension")
 
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "config", &common.Configuration{})
-	if err := cmd.ExecuteContext(ctx); err != nil {
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		os.Exit(1)
 	}
 }
