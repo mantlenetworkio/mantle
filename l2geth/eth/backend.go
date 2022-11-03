6e17159a3fbe682020cc91a20e8b728653736bc1
chengwenxi <22697326+chengwenxi@users.noreply.github.com>
2022-11-03 15:49:07 +0800

    refactor clique consensus

--- l2geth/eth/backend.go
+++ l2geth/eth/backend.go
@@ -553,6 +553,18 @@ func (s *Ethereum) Start(srvr *p2p.Server) error {
 
 	// Start the RPC service
 	s.netRPCService = ethapi.NewPublicNetAPI(srvr, s.NetVersion())
+
+	// Figure out a max peers count based on the server limits
+	maxPeers := srvr.MaxPeers
+	if s.config.LightServ > 0 {
+		if s.config.LightPeers >= srvr.MaxPeers {
+			return fmt.Errorf("invalid peer config: light peer count (%d) >= total peer count (%d)", s.config.LightPeers, srvr.MaxPeers)
+		}
+		maxPeers -= s.config.LightPeers
+	}
+	// Start the networking layer and the light server if requested
+	s.protocolManager.Start(maxPeers)
+
 	return nil
 }
 
