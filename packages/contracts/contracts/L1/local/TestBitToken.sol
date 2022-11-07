288befeb0d96fd1e56fd17c025ee087a8616de12
sha3n <qingruishen@yahoo.com>
2022-11-07 19:16:24 +0800

    [fix]: fix details

--- packages/contracts/contracts/L1/local/TestBitToken.sol
+++ packages/contracts/contracts/L1/local/TestBitToken.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BitTokenERC20 is ERC20 {
+
+    mapping(address => uint256) public mintRecord;
+    uint256 public mintGap;
+    address public owner = msg.sender;
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Mint 100 tokens to msg.sender
         // Similar to how
@@ -12,9 +17,24 @@ contract BitTokenERC20 is ERC20 {
         _mint(msg.sender, 10000 * 10**uint(decimals()));
     }
 
+    event MintAfterBlockHeight(uint256 height);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Function can only be called by the owner.");
+        _;
+    }
+
+    function setMintableGap(uint256 _mintGap) public onlyOwner {
+        mintGap = _mintGap;
+    }
+
     // public mint for any user
     function mint(uint256 amount) external {
         require(msg.sender != address(0), "ERC20: mint to the zero address");
-        _mint(msg.sender, amount);
+        if((mintRecord[msg.sender] == 0 || block.number - mintRecord[msg.sender] > mintGap) || ERC20(this).balanceOf(msg.sender) < 1000000000000000000000) {
+            mintRecord[msg.sender] = block.number;
+            _mint(msg.sender, amount);
+        }
+        emit MintAfterBlockHeight(mintRecord[msg.sender] + mintGap);
     }
 }
