bb6b85370f438adf5b5291d3bee93e20327de8c9
corey <94893343+curryxbo@users.noreply.github.com>
2022-11-16 13:07:20 +0800

    Update SequencerFeeVault: add Add modifiable parameters (#277)
    * update SequencerFeeVault: add Add modifiable parameters
    
    * fix
    
    * add function
    
    * revert json
    
    * fix owner
    
    * delete local json
    
    * fix .ignore

--- packages/contracts/deploy-config/mainnet.ts
+++ packages/contracts/deploy-config/mainnet.ts
@@ -14,6 +14,7 @@ const config = {
   bvmFeeWalletAddress: '0x391716d440c151c42cdf1c95c1d83a5427bca52c',
   bvmAddressManagerOwner: '0x9BA6e03D8B90dE867373Db8cF1A58d2F7F006b3A',
   bvmGasPriceOracleOwner: '0x7107142636C85c549690b1Aca12Bdb8052d26Ae6',
+  bvmFeeWalletOwner: '0x7107142636C85c549690b1Aca12Bdb8052d26Ae6',
   bvmWhitelistOwner: '0x648E3e8101BFaB7bf5997Bd007Fb473786019159',
 }
 
