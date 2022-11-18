8eb7aabc83324d345f1fb9fb30eb049d1139785b
Aodhgan Gleeson <aodhgan.gleeson@windranger.io>
2022-11-17 19:37:39 -0800

    udpate

--- packages/contracts/contracts/data-availability/gen_bindings.sh
+++ packages/contracts/contracts/data-availability/gen_bindings.sh
@@ -5,20 +5,22 @@ mkdir -p data
 
 dirname="EigenDataLayrRollup"
 for name in $dirname; do
-    echo $name
+
     mkdir -p ../common/contracts/bindings/${name}
 
     mkdir -p ./bindings/${name}/
+    touch ./bindings/${name}/binding.go
 
     dl_json="./out/${name}.sol/${name}.json"
     solc_abi=$(cat ${dl_json} | jq -r '.abi')
     solc_bin=$(cat ${dl_json} | jq -r '.bytecode.object')
     echo ${solc_abi} > data/DL.abi
     echo ${solc_bin} > data/DL.bin
-    echo "here"
+
     rm -f ../common/contracts/bindings/${name}/binding.go
-    echo "here1"
+
     abigen --bin=data/DL.bin --abi=data/DL.abi --pkg=contract${name} --out=./bindings/${name}/binding.go
+    echo "generated bindings for ${name}"
 done
 
 
