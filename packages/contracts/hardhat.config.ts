e283d4bcf006dc1c913ba2c99d7253664d660f77
corey <94893343+curryxbo@users.noreply.github.com>
2022-11-23 15:48:27 +0800

    Adjust charging logic (#317)
    * fix gasPrice
    
    * clean
    
    * revert local file
    
    * gasOracle add log
    
    * revert local file
    
    * fix gasPriceOracle contract modifier checkValue
    
    * fix gasOracleContract annotation

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -255,8 +255,12 @@ const config: HardhatUserConfig = {
       default: 6,
     },
     gasPriceOracleIsBurning: {
-      type: 'boolean',
-      default: true,
+      type: 'number',
+      default: 1,
+    },
+    gasPriceOracleCharge: {
+      type: 'number',
+      default: 0,
     },
     gasPriceOracleL1BaseFee: {
       type: 'number',
