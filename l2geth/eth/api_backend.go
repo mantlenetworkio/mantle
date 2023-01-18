f4c5abd1c12ae4594e104e1476a35666a1800340
curryxbo <curryx1992@gmail.com>
2023-01-18 15:20:20 +0800

    fix log

--- l2geth/eth/api_backend.go
+++ l2geth/eth/api_backend.go
@@ -306,7 +306,7 @@ func (b *EthAPIBackend) SendTx(ctx context.Context, signedTx *types.Transaction)
 		return nil
 	}
 	if b.eth.syncService.IsUpdateGasPriceTx(signedTx) {
-		return fmt.Errorf("sequencer does not accept updateGasPrice tx transactions. Please send to the correct sequencer address")
+		return fmt.Errorf("sequencer does not accept updateGasPrice transactions. Please send to the correct sequencer address")
 	}
 	to := signedTx.To()
 	if to != nil {
