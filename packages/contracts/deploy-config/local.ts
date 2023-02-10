02151a7d096162a07fca34baee840107f5d56222
wenbin <bradyjoestar@gmail.com>
2023-02-10 15:59:05 +0800

    wb:change height

--- packages/contracts/deploy-config/local.ts
+++ packages/contracts/deploy-config/local.ts
@@ -2,7 +2,7 @@ const config = {
   l1BlockTimeSeconds: 15,
   l2BlockGasLimit: 15_000_000,
   l2ChainId: 17,
-  fixBlockHashBranchingBlock: 110000,
+  fixBlockHashBranchingBlock: 10,
   ctcL2GasDiscountDivisor: 32,
   ctcEnqueueGasCost: 60_000,
   sccFaultProofWindowSeconds: 0,
