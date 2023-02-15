87dd097a8a5bd276ebfb0aec82b0e73592f422fe
curryxbo <curryx1992@gmail.com>
2023-02-15 14:18:45 +0800

    add da base Fee

--- l2geth/rollup/client_test.go
+++ l2geth/rollup/client_test.go
@@ -1,11 +1,21 @@
 package rollup
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"math/big"
 	"testing"
+	"time"
+
+	"github.com/influxdata/influxdb/pkg/testing/assert"
+	"github.com/mantlenetworkio/mantle/l2geth/accounts/abi/bind"
+	"github.com/mantlenetworkio/mantle/l2geth/common"
+	"github.com/mantlenetworkio/mantle/l2geth/core/types"
+	"github.com/mantlenetworkio/mantle/l2geth/crypto"
+	"github.com/mantlenetworkio/mantle/l2geth/ethclient"
 
 	"github.com/jarcoal/httpmock"
 )
@@ -73,3 +83,55 @@ func TestDecodedJSON(t *testing.T) {
 		t.Fatal("Cannot decode")
 	}
 }
+
+type ExtAcc struct {
+	Key  *ecdsa.PrivateKey
+	Addr common.Address
+}
+
+func FromHexKey(hexkey string) (ExtAcc, error) {
+	key, err := crypto.HexToECDSA(hexkey)
+	if err != nil {
+		return ExtAcc{}, err
+	}
+	pubKey := key.Public()
+	pubKeyECDSA, ok := pubKey.(*ecdsa.PublicKey)
+	if !ok {
+		err = fmt.Errorf("publicKey is not of type *ecdsa.PublicKey")
+		return ExtAcc{}, err
+	}
+	addr := crypto.PubkeyToAddress(*pubKeyECDSA)
+	return ExtAcc{key, addr}, nil
+}
+
+func TestBatchTransactions(t *testing.T) {
+	account, _ := FromHexKey("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+	txOpt := bind.NewKeyedTransactor(account.Key)
+
+	client, err := ethclient.Dial("http://localhost:8545")
+	assert.NoError(t, err)
+
+	receiveAccount := common.HexToAddress("0x76EFAac78C011D24BC975a5dAcC9a91245397e1a")
+	txOpt.Value = big.NewInt(5e18)
+	txOpt.GasLimit = uint64(21000)
+	txOpt.GasPrice = big.NewInt(1)
+
+	for i := 0; i < 1000; i++ {
+		nonce, err := client.PendingNonceAt(context.Background(), account.Addr)
+		assert.NoError(t, err)
+
+		txOpt.Value = big.NewInt(1).Mul(big.NewInt(1e18), big.NewInt(int64(i)))
+		rawTx := types.NewTransaction(nonce, receiveAccount, txOpt.Value, txOpt.GasLimit, txOpt.GasPrice, nil)
+
+		signedTx, err := txOpt.Signer(types.HomesteadSigner{}, txOpt.From, rawTx)
+		assert.NoError(t, err)
+
+		err = client.SendTransaction(context.Background(), signedTx)
+		assert.NoError(t, err)
+
+		t.Logf("index %d, txHash %s", i, signedTx.Hash().String())
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
