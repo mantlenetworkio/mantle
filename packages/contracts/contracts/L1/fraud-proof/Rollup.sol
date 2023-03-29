e755bf08e9605761c19c479a77b64ecd0916eed2
kukoomomo <kukoomomo@gmail.com>
2023-03-29 16:36:30 +0800

    clean

--- packages/contracts/contracts/L1/fraud-proof/Rollup.sol
+++ packages/contracts/contracts/L1/fraud-proof/Rollup.sol
@@ -430,7 +430,7 @@ contract Rollup is Lib_AddressResolver, RollupBase, Whitelist {
         if (loserStake > baseStakeAmount) {
             // If loser has a higher stake than the winner, refund the difference.
             // Loser gets deleted anyways, so maybe unnecessary to set amountStaked.
-            // stakers[loser].amountStaked = witannerStake;
+            // stakers[loser].amountStaked = winnerStake;
             withdrawableFunds[loser] += (loserStake - baseStakeAmount);
             amountWon = baseStakeAmount;
         } else {
