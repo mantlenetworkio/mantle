87eb7edee63931e5c3f1bc6f1bde475482032eae
sha3n <qingruishen@yahoo.com>
2023-04-24 10:44:31 +0800

    [fix]: change license

--- packages/contracts/contracts/L1/delegation/DelegationManagerStorage.sol
+++ packages/contracts/contracts/L1/delegation/DelegationManagerStorage.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
--- packages/contracts/contracts/L1/delegation/WhiteListBase.sol
+++ packages/contracts/contracts/L1/delegation/WhiteListBase.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 abstract contract Whitelist is OwnableUpgradeable {
