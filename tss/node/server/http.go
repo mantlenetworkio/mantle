c4c5d369bcaf5c4ddbcf6a23c5829c5db506295f
HaoyangLiu <lhy.0318@163.com>
2023-05-09 11:03:48 +0800

    remove useless code

--- tss/node/server/http.go
+++ tss/node/server/http.go
@@ -6,10 +6,8 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"strings"
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 	cmm "github.com/mantlenetworkio/mantle/tss/common"
 	sign "github.com/mantlenetworkio/mantle/tss/node/signer"
@@ -21,11 +19,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-)
-
-const (
-	jwtExpiryTimeout = 60 * time.Second
-	jwtKeyLength     = 32
 )
 
 type Server struct {
@@ -42,49 +35,6 @@ type GenRequest struct {
 }
 
 type MockEventRequest struct {
-}
-
-type jwtHandler struct {
-	keyFunc func(token *jwt.Token) (interface{}, error)
-	handler http.Handler
-}
-
-// ServeHTTP implements http.Handler
-func (handler *jwtHandler) ServeHTTP(out http.ResponseWriter, r *http.Request) {
-	var (
-		strToken string
-		claims   jwt.RegisteredClaims
-	)
-	if auth := r.Header.Get("Authorization"); strings.HasPrefix(auth, "Bearer ") {
-		strToken = strings.TrimPrefix(auth, "Bearer ")
-	}
-	if len(strToken) == 0 {
-		http.Error(out, "missing token", http.StatusUnauthorized)
-		return
-	}
-	// We explicitly set only HS256 allowed, and also disables the
-	// claim-check: the RegisteredClaims internally requires 'iat' to
-	// be no later than 'now', but we allow for a bit of drift.
-	token, err := jwt.ParseWithClaims(strToken, &claims, handler.keyFunc,
-		jwt.WithValidMethods([]string{"HS256"}),
-		jwt.WithoutClaimsValidation())
-
-	switch {
-	case err != nil:
-		http.Error(out, err.Error(), http.StatusUnauthorized)
-	case !token.Valid:
-		http.Error(out, "invalid token", http.StatusUnauthorized)
-	case !claims.VerifyExpiresAt(time.Now(), false): // optional
-		http.Error(out, "token is expired", http.StatusUnauthorized)
-	case claims.IssuedAt == nil:
-		http.Error(out, "missing issued-at", http.StatusUnauthorized)
-	case time.Since(claims.IssuedAt.Time) > jwtExpiryTimeout:
-		http.Error(out, "stale token", http.StatusUnauthorized)
-	case time.Until(claims.IssuedAt.Time) > jwtExpiryTimeout:
-		http.Error(out, "future token", http.StatusUnauthorized)
-	default:
-		handler.handler.ServeHTTP(out, r)
-	}
 }
 
 func NewHttpServer(addr string, t tsslib.Server, signer *sign.Processor, nonProd bool, jwtSecretStr string) (*Server, error) {
