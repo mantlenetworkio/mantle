c3dd7159bd0f8401d4f0c7564db9b66b71c8585f
Shijiang Guo <guoshijiang2012@163.com>
2023-07-19 19:08:52 +0800

    [R4R]-{develop}: fix l2geth error judge (#1307)
    fix secure3 audit issure for err judge

--- l2geth/core/state_transition.go
+++ l2geth/core/state_transition.go
@@ -178,7 +178,7 @@ func NewStateTransition(evm *vm.EVM, msg Message, gp *GasPool) (*StateTransition
 // state and would never be accepted within a block.
 func ApplyMessage(evm *vm.EVM, msg Message, gp *GasPool) ([]byte, uint64, bool, error) {
 	stateTransition, err := NewStateTransition(evm, msg, gp)
-	if err == nil {
+	if err != nil {
 		log.Error("apply message fall", "err", err)
 		return nil, 0, false, err
 	}
